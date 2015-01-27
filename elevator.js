magwo/elevatorsaga

commit ac29c30712f587d18c82c44cbf0193b916b00e33

Added the ability to change number of users per elevator

elevator.js
@@ -1,12 +1,13 @@
 
 
-var asElevator = function(movable, speedFloorsPerSec, floorCount, floorHeight) {
+var asElevator = function(movable, speedFloorsPerSec, floorCount, floorHeight, maxUsers) {
     var elevator = movable;
 
     var ACCELERATION = floorHeight * 2.1;
     var DECELERATION = floorHeight * 2.6;
     var MAXSPEED = floorHeight * speedFloorsPerSec;
 
+    elevator.maxUsers = maxUsers || 4;
     elevator.destinationY = 0.0;
     elevator.velocityY = 0.0;
     // isMoving flag is needed when going to same floor again - need to re-raise events
@@ -20,12 +21,10 @@ var asElevator = function(movable, speedFloorsPerSec, floorCount, floorHeight) {
     elevator.buttonStates = _.map(_.range(floorCount), function(e, i){ return false; });
     elevator.moveCount = 0;
     elevator.removed = false;
-    elevator.userSlots = [
-        {pos: [2, 30], user: null},
-        {pos: [12, 30], user: null},
-        {pos: [22, 30], user: null},
-        {pos: [32, 30], user: null}];
-
+    elevator.userSlots = _.map(_.range(elevator.maxUsers), function(user, i) {
+        return { pos: [2 + (i * 10), 30], user: null};
+    });
+    elevator.width = elevator.maxUsers * 10;
 
     elevator.setFloorPosition = function(floor) {
         var destination = elevator.getYPosOfFloor(floor);
@@ -175,7 +174,7 @@ var asElevator = function(movable, speedFloorsPerSec, floorCount, floorHeight) {
 
     elevator.getLoadFactor = function() {
         var load = _.reduce(elevator.userSlots, function(sum, slot) { return sum + (slot.user ? slot.user.weight : 0); }, 0);
-        return load / 400.0;
+        return load / elevator.maxUsers * 100;
     }
 
 

index.html
@@ -43,7 +43,7 @@
     </script>
 
     <script type="text/template" id="elevator-template">
-        <div class="elevator movable" style="left: {e.worldX}px; top: {e.worldY}px">
+        <div class="elevator movable" style="left: {e.worldX}px; top: {e.worldY}px; width: {e.width}px">
             <span class="directionindicator directionindicatorup"><i class="fa fa-arrow-circle-up up activated"></i></span>
             <span class="floorindicator">{e.currentFloor}</span>
             <span class="directionindicator directionindicatordown"><i class="fa fa-arrow-circle-down down activated"></i></span>

style.css
@@ -246,7 +246,6 @@ button.right {
 .elevator {
     background-color: #4F8686;
     border: 2px solid white;
-    width: 40px;
     height: 46px;
     z-index: 1;
 }

world.js
@@ -12,12 +12,13 @@ var createWorldCreator = function() {
         });
         return floors;
     };
-    creator.createElevators = function(elevatorCount, floorCount, floorHeight) {
+    creator.createElevators = function(elevatorCount, floorCount, floorHeight, maxUsers) {
         var elevators = _.map(_.range(elevatorCount), function(e, i) {
             var elevator = asMovable({});
+            elevator = asElevator(elevator, 2.6, floorCount, floorHeight, maxUsers);
+
             // Move to right x position
-            elevator.moveTo(200+60*i, null);
-            elevator = asElevator(elevator, 2.6, floorCount, floorHeight);
+            elevator.moveTo((20 + elevator.width) * i + 200, null);
             elevator.setFloorPosition(0);
             elevator.updateDisplayPosition();
             return elevator;
@@ -69,7 +70,7 @@ var createWorldCreator = function() {
         
         
         world.floors = creator.createFloors(options.floorCount, world.floorHeight);
-        world.elevators = creator.createElevators(options.elevatorCount, options.floorCount, world.floorHeight);
+        world.elevators = creator.createElevators(options.elevatorCount, options.floorCount, world.floorHeight, options.maxUsers);
         world.elevatorInterfaces = _.map(world.elevators, function(e) { return asElevatorInterface({}, e, options.floorCount); });
         world.users = [];
         world.transportedCounter = 0;

commit 1f2d3f08bc25895299208a5f0c21f8e0a9b396d2

Correctly calculate elevator loadFactor
This was broken by ac29c30712f587d18c82c44cbf0193b916b00e33, which
introduced an order of operations bug when updating this function to
change the max number of users per elevator.

elevator.js
@@ -185,7 +185,7 @@ var asElevator = function(movable, speedFloorsPerSec, floorCount, floorHeight, m
 
     elevator.getLoadFactor = function() {
         var load = _.reduce(elevator.userSlots, function(sum, slot) { return sum + (slot.user ? slot.user.weight : 0); }, 0);
-        return load / elevator.maxUsers * 100;
+        return load / (elevator.maxUsers * 100);
     }
 
 