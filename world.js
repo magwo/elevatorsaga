c12543196e98f567fb91e1d0d47d6a4101ebbd9c
Andrew Potter <apottere@gmail.com>
2015-02-07 00:14:58 -0500

    Allow users to re-enter elevator that changes indicators without moving.

--- world.js
+++ world.js
@@ -120,6 +120,12 @@ var createWorldCreator = function() {
                     }
                 });
             });
+            elevator.on("indicatorstate_change", function() {
+                var elevator = this;
+                if(elevator.isOnAFloor() && !elevator.isMoving && !elevator.isFull()) {
+                    elevator.trigger("entrance_available");
+                }
+            });
         });
 
         // This will cause elevators to "re-arrive" at floors if someone presses an
