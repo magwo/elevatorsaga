0492e65dd1208edf744ab82acb95bcfbc23e661c
Magnus Wolffelt <magnus.wolffelt@gmail.com>
2015-04-11 10:41:26 +0200

    Remove unused Promise implementation

--- base.js
+++ base.js
@@ -48,36 +48,6 @@ var createBoolPassthroughFunction = function(owner, obj, objPropertyName) {
     };
 };
 
-
-// Simple Promise pattern using riot.observable
-function Promise() {
-    var self = riot.observable(this);
-    self.resolution = null;
-    self.result = null;
-    $.map(['done', 'fail', 'always'], function(name) {
-        self[name] = function(arg) {
-            if($.isFunction(arg)) {
-                self.one(name, arg);
-                if(self.resolution === name || (self.resolution !== null && name === 'always')) {
-                    self.trigger(name, self.result);
-                }
-            } else {
-                if(name === 'always') {
-                    throw new Error('Invalid argument for "always" function: ' + arg);
-                }
-                if(self.resolution !== null) {
-                    throw new Error('Can not resolve promise - already resolved to ' + self.resolution);
-                }
-                self.resolution = name;
-                self.result = arg;
-                self.trigger(name, self.result).trigger('always', self.result);
-            }
-            return self;
-        };
-    });
-}
-
-
 window.distanceNeededToAchieveSpeed = function(currentSpeed, targetSpeed, acceleration) {
     // v² = u² + 2a * d
     var requiredDistance = (Math.pow(targetSpeed, 2) - Math.pow(currentSpeed, 2)) / (2 * acceleration);
